f3312ac357688ec86c8688aadec59b8c19b60382
Jacob Brower <jacobbrower@jacobs-macbook-pro.local>
2018-11-09 02:05:29 -0500

    Initial Commit

---
+++ Truck Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Truck Tracker
+//
+//  Created by Jacob Brower on 11/9/18.
+//  Copyright © 2018 Dorm Creation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
